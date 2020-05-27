d6d0aef6d37b83c6ecf9ed7192e5b12f0a8dc8e0
Spencilian <spencerli253@gmail.com>
2020-05-27 09:38:50 -0400

    Began work on api

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
 const { startDB, } = require('./database/mongo')
 const { updateVote, } = require('./database/mealEntries')
 
+const app = express()
+
+// God im so mature
+const port = 6969
+
+app.use(helmet())
+app.use(cors())
+app.use(morgan('combined'))
+
+app.post('/api/', async (req, res) => {
+    
+})
+
 startDB().then(
     async () => {
-        await updateVote("3000")
+        await updateVote(
+            {year: 2020, month: 05, day: 27, meal: 0}
+        )
     }
 )
